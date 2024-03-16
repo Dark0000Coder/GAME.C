3c109fd0d041b1e0c2d8c554b0b161df58584c06
Dark0000Coder <luckyrocky340@gmail.com>
2024-03-16 16:08:15 +0530

    Add Carrom Code

---
+++ Carrom_Design_in_Pattern_Form.c
@@ -0,0 +1,308 @@
+#include <stdio.h>
+
+int main() 
+{
+    int rows, columns, i, j;
+
+    printf("Enter number of rows=");
+    scanf("%d",&rows);
+    printf("Enter number of columns=");
+    scanf("%d",&columns);
+
+   for (i = 1; i <= rows; i++) 
+    {
+        for (j = 1; j <= columns; j++) 
+        {
+            printf("  ");
+        }
+        for (j = columns; j >= 1; j--) 
+        {
+            printf("* ");
+        }
+        for (j = 1; j <= columns; j++) 
+        {
+            printf("* ");
+        }
+        for (j = 1; j <= columns; j++) 
+        {
+            printf("* ");
+        }
+        for (j = columns; j >= 1; j--) 
+        {
+            printf("* ");
+        }
+        for (j = 1; j <= columns; j++) 
+        {
+            printf("* ");
+        }
+        for (j = 1; j <= columns; j++) 
+        {
+            printf("* ");
+        }
+        for (j = columns; j >= 1; j--) 
+        {
+            printf("  ");
+        }
+        printf("\n");
+    }
+
+   for (i = rows; i >= 1; i--) 
+    {
+        for (j = columns; j >= 1; j--) 
+        {
+            printf("* ");
+        }
+        for (j = 1; j <= columns; j++)
+        {
+            printf("  ");
+        }
+        for (j = columns; j >= 1; j--)
+        {
+            printf("  ");
+        }
+        for (j = columns; j >= 1; j--) 
+        {
+            printf("  ");
+        }
+        for (j = 1; j <= columns; j++)
+        {
+            printf("  ");
+        }
+        for (j = columns; j >= 1; j--)
+        {
+            printf("  ");
+        }
+        for (j = columns; j >= 1; j--) 
+        {
+            printf("  ");
+        }
+        for (j = 1; j <= columns; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    for (i = rows; i >= 1; i--) 
+    {
+        for (j = columns; j >= 1; j--) 
+        {
+            printf("* ");
+        }
+        for (j = columns; j >= 1; j--)
+        {
+            printf("  ");
+        }
+        for (j = 1; j <= columns; j++)
+        {
+            printf("* ");
+        }
+        for (j = columns; j >= 1; j--) 
+        {
+            printf("* ");
+        }
+        for (j = columns; j >= 1; j--)
+        {
+            printf("* ");
+        }
+        for (j = 1; j <= columns; j++)
+        {
+            printf("* ");
+        }
+        for (j = columns; j >= 1; j--) 
+        {
+            printf("  ");
+        }
+        for (j = columns; j >= 1; j--)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    
+   for (i = 1; i <= rows; i++) 
+    {
+        for (j = 1; j <= columns; j++) 
+        {
+            printf("* ");
+        }
+        for (j = columns; j >= 1; j--) 
+        {
+            printf("  ");
+        }
+        for (j = 1; j <= columns; j++) 
+        {
+            printf("* ");
+        }
+        for (j = 1; j <= columns; j++) 
+        {
+            printf("  ");
+        }
+        for (j = columns; j >= 1; j--) 
+        {
+            printf("  ");
+        }
+        for (j = 1; j <= columns; j++) 
+        {
+            printf("* ");
+        }
+        for (j = 1; j <= columns; j++) 
+        {
+            printf("  ");
+        }
+        for (j = columns; j >= 1; j--) 
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+   for (i = rows; i >= 1; i--) 
+    {
+        for (j = columns; j >= 1; j--) 
+        {
+            printf("* ");
+        }
+        for (j = 1; j <= columns; j++)
+        {
+            printf("  ");
+        }
+        for (j = columns; j >= 1; j--)
+        {
+            printf("* ");
+        }
+        for (j = columns; j >= 1; j--) 
+        {
+            printf("  ");
+        }
+        for (j = 1; j <= columns; j++)
+        {
+            printf("  ");
+        }
+        for (j = columns; j >= 1; j--)
+        {
+            printf("* ");
+        }
+        for (j = columns; j >= 1; j--) 
+        {
+            printf("  ");
+        }
+        for (j = 1; j <= columns; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    for (i = rows; i >= 1; i--) 
+    {
+        for (j = columns; j >= 1; j--) 
+        {
+            printf("* ");
+        }
+        for (j = columns; j >= 1; j--)
+        {
+            printf("  ");
+        }
+        for (j = 1; j <= columns; j++)
+        {
+            printf("* ");
+        }
+        for (j = columns; j >= 1; j--) 
+        {
+            printf("* ");
+        }
+        for (j = columns; j >= 1; j--)
+        {
+            printf("* ");
+        }
+        for (j = 1; j <= columns; j++)
+        {
+            printf("* ");
+        }
+        for (j = columns; j >= 1; j--) 
+        {
+            printf("  ");
+        }
+        for (j = columns; j >= 1; j--)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+ 
+    for (i = 1; i <= rows; i++) 
+    {
+        for (j = 1; j <= columns; j++) 
+        {
+            printf("* ");
+        }
+        for (j = columns; j >= 1; j--) 
+        {
+            printf("  ");
+        }
+        for (j = 1; j <= columns; j++) 
+        {
+            printf("  ");
+        }
+        for (j = 1; j <= columns; j++) 
+        {
+            printf("  ");
+        }
+        for (j = columns; j >= 1; j--) 
+        {
+            printf("  ");
+        }
+        for (j = 1; j <= columns; j++) 
+        {
+            printf("  ");
+        }
+        for (j = 1; j <= columns; j++) 
+        {
+            printf("  ");
+        }
+        for (j = columns; j >= 1; j--) 
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+   for (i = rows; i >= 1; i--) 
+    {
+        for (j = columns; j >= 1; j--) 
+        {
+            printf("  ");
+        }
+        for (j = 1; j <= columns; j++)
+        {
+            printf("* ");
+        }
+        for (j = columns; j >= 1; j--)
+        {
+            printf("* ");
+        }
+        for (j = columns; j >= 1; j--) 
+        {
+            printf("* ");
+        }
+        for (j = 1; j <= columns; j++)
+        {
+            printf("* ");
+        }
+        for (j = columns; j >= 1; j--)
+        {
+            printf("* ");
+        }
+        for (j = columns; j >= 1; j--) 
+        {
+            printf("* ");
+        }
+        for (j = 1; j <= columns; j++)
+        {
+            printf("  ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
